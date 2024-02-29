d39df670ff98041ce99fcab3572fcb5824ffaafe
hongdahyeon <hyeon8287@gmail.com>
2024-02-29 10:53:13 +0900

    commit 1 : 공통 템플릿 및 css,js 세팅

---
+++ src/main/resources/static/assets/js/menu/menu.js
@@ -0,0 +1,14 @@
+
+/* side bar menu toggle js file */
+window.addEventListener('DOMContentLoaded', event => {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
