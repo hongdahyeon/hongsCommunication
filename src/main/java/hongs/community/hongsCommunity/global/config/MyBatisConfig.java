4047d5323a4cf9a2367e233a6ed6d2e24cd052f7
hongdahyeon <hyeon8287@gmail.com>
2024-03-13 14:42:42 +0900

    commit 18: mybatis-config 파일과 MyBatisConfig 파일 합침

--- src/main/java/hongs/community/hongsCommunity/global/config/MyBatisConfig.java
+++ src/main/java/hongs/community/hongsCommunity/global/config/MyBatisConfig.java
@@ -1,16 +1,17 @@
 package hongs.community.hongsCommunity.global.config;
 
-import groovy.lang.Interceptor;
 import hongs.community.hongsCommunity.global.interceptor.MybatisInterceptor;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionManager;
 
 import javax.sql.DataSource;
@@ -23,7 +24,7 @@ import javax.sql.DataSource;
 * @summary   Mybatis에서 SQL구문은 SqlSession을 통해 실행되며 thread마다 별도의 SqlSession 인스턴스를 갖게 된다.
 **/
 
-@Configuration
+@Component
 public class MyBatisConfig {
 
     @Value("${spring.datasource.driver-class-name}")
@@ -39,7 +40,7 @@ public class MyBatisConfig {
     private String password;
 
     @Value("${hong.mybatis-setting.config-location}")
-    private String configLocation;
+    private String configLocation;                  // -> MybatisConfig 파일에서 코드 작성으로 대체
 
     @Value("${hong.mybatis-setting.mapper-locations}")
     private String mapperLocations;
@@ -61,18 +62,35 @@ public class MyBatisConfig {
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
         SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 
+        // 1. DB 정보
         factoryBean.setDataSource(dataSource);
-        factoryBean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource(configLocation));
 
+        // 2. mybatis-config.xml 위치
+        // factoryBean.setConfigLocation(new PathMatchingResourcePatternResolver().getResource(configLocation));
+
+        // 3. mapper 위치
         Resource[] resources = new PathMatchingResourcePatternResolver().getResources(mapperLocations);
         factoryBean.setMapperLocations(resources);
 
+        // 4. dto, vo 위치
         factoryBean.setTypeAliasesPackage(typeAliasesPackage);
 
+        // 5. plugins: interceptor
         MybatisInterceptor mybatisInterceptor = new MybatisInterceptor();
         factoryBean.setPlugins(mybatisInterceptor);
 
+        // 6. settings
+        factoryBean.setConfiguration(setConfiguration());
+
         return factoryBean.getObject();
+    }
+
+    public Configuration setConfiguration() {
+        Configuration configuration = new  Configuration();
+        configuration.setJdbcTypeForNull(JdbcType.VARCHAR);
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.getTypeAliasRegistry().registerAlias("Long", Long.class);
+        return configuration;
     }
 
     @Bean(name = "transactionManager")
