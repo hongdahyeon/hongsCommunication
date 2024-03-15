950854d51bd48b02c5613aecfb0d6bef759e6001
hongdahyeon <hyeon8287@gmail.com>
2024-03-15 14:53:03 +0900

    commit 22 1. 리퍼러 정책 : same-origin 추가 2. cors 정책 추가 3. xss 공격 : 기본 xss protection 적용

--- src/main/java/hongs/community/hongsCommunity/global/config/SecurityConfig.java
+++ src/main/java/hongs/community/hongsCommunity/global/config/SecurityConfig.java
@@ -3,11 +3,16 @@ package hongs.community.hongsCommunity.global.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity              // spring security 활성화 어노테이션
@@ -28,6 +33,12 @@ public class SecurityConfig {
         http
 //            .csrf(AbstractHttpConfigurer::disable)      // csrf disable 처리
             .csrf(cs -> cs.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+            .cors(corsConfigurer -> corsConfigurer.configurationSource(request -> corsConfiguration()))
+            .headers(header ->
+                       header
+                           .xssProtection(Customizer.withDefaults())
+                           .referrerPolicy(policyConfig -> policyConfig.policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN))
+            )
             .authorizeHttpRequests(authorize ->
                 authorize
                         .requestMatchers(SecurityConfigMatchers.BEFORE_LOGIN).permitAll()
@@ -50,5 +61,15 @@ public class SecurityConfig {
                     .clearAuthentication(true)
             );
         return http.build();
+    }
+
+    private CorsConfiguration corsConfiguration(){
+        CorsConfiguration cors = new CorsConfiguration();
+        cors.setAllowedOrigins(List.of("*"));       // 모든 출처 허용
+        cors.setAllowedMethods(List.of("*"));       // 모든 http 메서드 허용
+        cors.setAllowedHeaders(List.of("*"));       // 모든 헤더 허용
+        cors.setAllowCredentials(true);                 // 자격 증명 허용 => 웹 브라우저가 요청을 보낼 때 쿠키와 HTTP 인증 정보를 함께 보내는지 여부를 결정한다.
+        cors.setMaxAge(3600L);                          // pre-flight 요청 캐시 시간 (초)
+        return cors;
     }
 }
